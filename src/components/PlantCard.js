9fba6541c28e077c75511edfb88e876d2b004ae4
Kane Mathis <admin@kanemathis.com>
2022-07-07 16:48:05 -0400

    deliverable 1 completed

--- src/components/PlantCard.js
+++ src/components/PlantCard.js
@@ -1,11 +1,13 @@
 import React from "react";
 
-function PlantCard() {
+function PlantCard({plantList}) {
+  const {id, name, image, price}=plantList
+
   return (
     <li className="card">
-      <img src={"https://via.placeholder.com/400"} alt={"plant name"} />
-      <h4>{"plant name"}</h4>
-      <p>Price: {"plant price"}</p>
+      <img src={image} alt={"plant name"} />
+      <h4>{name}</h4>
+      <p>Price: {price}</p>
       {true ? (
         <button className="primary">In Stock</button>
       ) : (
